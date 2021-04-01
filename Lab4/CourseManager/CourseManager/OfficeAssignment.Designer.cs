058be48755a55e8fb9f7a55537cc96dc51991d7c
Andrew Ryabous <erdandrey@yandex.ru>
2021-04-01 22:31:15 +0300

    last tasks

---
+++ Lab4/CourseManager/CourseManager/OfficeAssignment.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace CourseManager
+{
+    partial class OfficeAssignment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.officeGridView = new System.Windows.Forms.DataGridView();
+            this.SaveChanges = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.officeGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // officeGridView
+            // 
+            this.officeGridView.AllowUserToAddRows = false;
+            this.officeGridView.AllowUserToDeleteRows = false;
+            this.officeGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.officeGridView.Location = new System.Drawing.Point(12, 12);
+            this.officeGridView.Name = "officeGridView";
+            this.officeGridView.Size = new System.Drawing.Size(392, 176);
+            this.officeGridView.TabIndex = 0;
+            // 
+            // SaveChanges
+            // 
+            this.SaveChanges.Location = new System.Drawing.Point(176, 194);
+            this.SaveChanges.Name = "SaveChanges";
+            this.SaveChanges.Size = new System.Drawing.Size(75, 23);
+            this.SaveChanges.TabIndex = 1;
+            this.SaveChanges.Text = "Update";
+            this.SaveChanges.UseVisualStyleBackColor = true;
+            this.SaveChanges.Click += new System.EventHandler(this.SaveChanges_Click);
+            // 
+            // OfficeAssignment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 229);
+            this.Controls.Add(this.SaveChanges);
+            this.Controls.Add(this.officeGridView);
+            this.Name = "OfficeAssignment";
+            this.Text = "OfficeAssignment";
+            this.Load += new System.EventHandler(this.OfficeAssignment_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.officeGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView officeGridView;
+        private System.Windows.Forms.Button SaveChanges;
+    }
+}
